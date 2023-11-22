f3571e6971b4dc13c17f842d06888b5be213c3ae
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-22 15:22:17 +0100

    Print to page all information off hotels

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <?php include 'hotel_list.php'; ?>
     <title>PHP Hotel</title>
 </head>
-<body>  
-    prova c
+<body> 
+    <ul class="hotels-list">
+        <?php foreach ($hotels as $hotel ) {
+            ?><li>
+                <h2><?php echo $hotel['name'] ?></h2>
+                <ul class="hotel-descriptions>">
+                    <?php foreach ($hotel as $key => $value) {
+                        ?>
+                            <li> 
+                                <span><?php echo $key . " :"?></span>
+                                <span><?php echo $value ?></span>
+                            </li>
+                    <?php } ?>
+                </ul>
+            </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
+
